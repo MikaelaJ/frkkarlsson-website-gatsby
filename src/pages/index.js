b8467844159495eba3640b5d2a4fd0ce45ee5776
“MikaelaJ” <Mikaela_johansson@hotmail.com>
2019-08-28 10:53:10 +0200

    components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from '../components/layout'
 
 export default () => (
-    <div>
+    <Layout>
     <h1>Frk Karlsson</h1>
         <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
 <div>Hello index!</div>
-    </div>
+    </Layout>
 )
