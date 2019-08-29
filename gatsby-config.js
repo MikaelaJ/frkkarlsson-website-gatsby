6dbbd09310190398d23bd73309364c7bc816f3fe
“MikaelaJ” <Mikaela_johansson@hotmail.com>
2019-08-29 13:28:10 +0200

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@
 module.exports = {
   /* Your site config here */
   plugins:[
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-146650914-1",
+      },
+    },
   ]
 }
